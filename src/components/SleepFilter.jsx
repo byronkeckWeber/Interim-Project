f9c823c4189b8615431befe47cce73f180be5bc7
byronkeckWeber <byronkeck@mail.weber.edu>
2026-09-20 16:06:46 -0600

    Update SleepFilter.jsx

--- src/components/SleepFilter.jsx
+++ src/components/SleepFilter.jsx
@@ -7,6 +7,7 @@ function SleepFilter(props) {
         <div>
             <label>Filter by Wake up Methods</label>
             <select onChange={dropDownChangeHandler} value={props.selected}>
+                <option value="All">All</option>
                 <option value="Himself">They woke up by themselves.</option>
                 <option value="WokeHimUp">I woke them up.</option>
                 <option value="LoudNoise">A loud noise woke them up.</option>
